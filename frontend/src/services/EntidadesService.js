9266d3dc46615b1bf669a2317e22f75345bb178a
jprinmobiliariauniversal <124696997+JuanesAcevedoP@users.noreply.github.com>
2025-11-23 22:21:47 -0500

    fix: Consumo de back servicios

--- frontend/src/services/EntidadesService.js
+++ frontend/src/services/EntidadesService.js
@@ -1,7 +1,7 @@
 import API_BASE_URL from "../config/api";
 
 export async function getEntidades() {
-  const response = await fetch(`${API_BASE_URL}/entidades`);
+  const response = await fetch(`${API_BASE_URL}/servicios`);
   if (!response.ok) throw new Error("Error al obtener entidades");
   return response.json();
 }
