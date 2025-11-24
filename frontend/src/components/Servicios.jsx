8cf44c3b86bff0e1b4378d9428eb4c9a573a890f
celene12 <celeneparra12@gmail.com>
2025-11-24 00:20:01 -0500

    Se agregaron los gif

--- frontend/src/components/Servicios.jsx
+++ frontend/src/components/Servicios.jsx
@@ -2,15 +2,26 @@ import { useState } from "react";
 import "../assets/css/Servicios.css";
 import logo_serv_uno from "../assets/img/ServicioNumeroUno.jpeg";
 import logo_serv_dos from "../assets/img/ServicioNumeroDos.jpeg";
+import gif_Interpretes from "../assets/gif/interpretes.gif";
+import gif_Entidades from "../assets/gif/Entidades.gif";
 import { Link } from "react-router-dom";
+import "../assets/css/Gif.css";
+
 
 export default function Servicios() {
+  const [search, setSearch] = useState("");
+   const [showEntidadesGif, setShowEntidadesGif] = useState(false);
+   const [showInterpretesGif, seShowInterpretesGif] = useState(false);
   return (
     <div id="container_servicios">
-        <section id="servicio_numero_uno">
+        <section 
+            onMouseEnter={() => seShowInterpretesGif(true)} 
+            onMouseLeave={() => seShowInterpretesGif(false)}
+            id="servicio_numero_uno">
             <div id="img_servicio_uno">
                 <img src={logo_serv_uno} alt="" />
             </div>
+            
             <div id="info_servicio_uno">
                 <p>Accede a un directorio de intérpretes de Lengua de Señas Colombiana, filtrados por especialidad, disponibilidad y zona.</p>
                 <Link to="/interpretes" id="info_servicio_button_link">
@@ -18,10 +29,23 @@ export default function Servicios() {
                 </Link>
             </div>
         </section>
-        <section id="servicio_numero_dos">
+        {showInterpretesGif && (
+          <div className="gif-container interprete">
+            <img 
+              src={gif_Interpretes} 
+              className="hover-gif"
+            />
+          </div>
+        )}
+        
+        <section
+              onMouseEnter={() => setShowEntidadesGif(true)} 
+              onMouseLeave={() => setShowEntidadesGif(false)}
+              id="servicio_numero_dos">
             <div id="img_servicio_dos">
                 <img src={logo_serv_dos} alt="" />   
             </div>
+            
             <div id="info_servicio_dos">
                 <h2>Contactarse con entidad</h2>
                 <p>Ubica hospitales y centros de salud accesibles en Medellín, con información de contacto directo y servicios inclusivos.</p>
@@ -30,6 +54,15 @@ export default function Servicios() {
                  </Link>
             </div>
         </section>
+        {showEntidadesGif && (
+          <div className="gif-container entidades">
+            <img 
+              src={gif_Entidades} 
+              className="hover-gif"
+            />
+          </div>
+        )}
+        
     </div>
   );
 }
--- frontend/src/pages/InicioPage.jsx
+++ frontend/src/pages/InicioPage.jsx
@@ -1,17 +1,37 @@
 import Nav from "../components/Nav";
 import "../assets/css/InicioPage.css";
 import Servicios from "../components/Servicios";
+import "../assets/css/Gif.css";
 import Footer from "../components/FooterDeLaPagina";
 import img_SN from "../assets/img/imagenSobreNosotros.jpeg";
+import gif_SN from "../assets/gif/Quien_somos.gif";
+import gif_Servicios from "../assets/gif/Servicios.gif";
+import { useState } from "react";
 
 export default function InicioPage() {
+ const [search, setSearch] = useState("");
+ const [showNosotrosGif, setShowNosotrosGif] = useState(false);
+ const [showServiciosGif, setShowServiciosGif] = useState(false);
   return (
     <>
       <Nav />
       <main>
         <div id="container_info_nosotros">
             <section id="sobreNosotros">
-              <h1>Sobre Nosotros</h1>
+              <span
+                onMouseEnter={() => setShowNosotrosGif(true)} 
+                onMouseLeave={() => setShowNosotrosGif(false)}
+              >
+                <h1>Sobre Nosotros</h1>
+              </span>
+              {showNosotrosGif && (
+                <div className="gif-container">
+                  <img 
+                    src={gif_SN} 
+                    className="hover-gif"
+                  />
+                </div>
+              )}
               <p>Una aplicación web que conecta a la comunidad sorda de Medellín con 
                 servicios accesibles, intérpretes de Lengua de Señas Colombiana 
                 y un sistema de emergencias inclusivo.</p>
@@ -20,7 +40,24 @@ export default function InicioPage() {
               <img src={img_SN} alt="Imagen sobre nosotros" id="Img" />
             </article>
         </div>
-        <h1 id="titulo_servicios">Servicios</h1>
+        
+       
+        <h1 
+          onMouseEnter={() => setShowServiciosGif(true)} 
+          onMouseLeave={() => setShowServiciosGif(false)}
+          id="titulo_servicios"
+        >
+          Servicios
+        </h1>
+        {showServiciosGif && (
+          <div className="gif-container servicios">
+            <img 
+              src={gif_Servicios} 
+              className="hover-gif"
+            />
+          </div>
+        )} 
+    
         <Servicios />
       </main>
       <Footer />
