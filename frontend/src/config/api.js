9d4989fc66e4e84519b4384f967a086a1044fca0
jprinmobiliariauniversal <124696997+JuanesAcevedoP@users.noreply.github.com>
2025-11-23 21:45:27 -0500

    chore: add Docker setup al front + backend

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,5 +1,2 @@
-const API_BASE_URL = import.meta.env.PROD
-  ? "https://mysignproject.onrender.com/api"
-  : "http://localhost:8000";
-
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
 export default API_BASE_URL;
